0f105d56a323764a07e9fe1e464f50dbd929cbe0
JoaoColeoni <60195895+JoaoColeoni@users.noreply.github.com>
2024-03-19 16:51:59 -0300

    Add backup e Readme
    atualização do Readme e adição dos arquivos de banco de dados de backup e inicialização do projeto

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
+#Sistema-Cadastros-PHP
+Esse projeto é um sistema que simula uma area administrativa onde você pode cadastrar/editar/excluir/consultar Clientes.
+
 # PHP
 `versão 7.4.33`
 
 # Banco de dados MySQL
 `versão 8.3.0`
 
-
+# Inicializar Projeto
+1 - Crie um Schema no banco de dados.
+2 - Restaure o backup das 3 tabelas da pasta `db` na raiz do projeto.
+3 - Crie uma copia do arquivo `config_template.php` e renomeie o arquivo para `config.php`.
+4 - Altere os dados do arquivo `config.php` com as informações do seu banco de dados MySQL e na variavel `$SITE` coloque o IP e Porta caso utilize, se for conexão local pode ser mantido o `http://localhost/`.
+5 - Acesse o arquivo `index.php` e acesse o sistema usando as credenciais de algum usuário da tabela `usuarios` (No backup já é inserido por padrão o usuário Login:`admin` e Senha:`12345`).
+6 - Utilize o Sistema como desejar.
---
+++ db/kabum_teste_cliente_endereco.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: kabum_teste
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente_endereco`
+--
+
+DROP TABLE IF EXISTS `cliente_endereco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente_endereco` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `cliente_id` int DEFAULT NULL,
+  `cep` varchar(8) DEFAULT NULL,
+  `rua` varchar(50) NOT NULL,
+  `bairro` varchar(50) NOT NULL,
+  `numero` varchar(10) DEFAULT NULL,
+  `complemento` varchar(50) DEFAULT NULL,
+  `cidade` varchar(60) NOT NULL,
+  `estado` varchar(2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cliente_id_idx` (`cliente_id`),
+  CONSTRAINT `cliente_id` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente_endereco`
+--
+
+LOCK TABLES `cliente_endereco` WRITE;
+/*!40000 ALTER TABLE `cliente_endereco` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cliente_endereco` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-19 16:44:05
